d72090dec085b3f30708297b2ea68162c2552699
Dhruv Sharma <dhruvsh5467@gmail.com>
2025-06-12 10:56:41 +0530

    navbar complete

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import {CircleUserRound} from 'lucide-react'
+import {CircleUserRound, Search} from 'lucide-react'
+import { DropdownMenu, DropdownMenuItem, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuSeparator } from "./ui/dropdown-menu";
 export default function Navbar() {
     return (
         <div className="flex justify-between items-center px-5 md:px-16 py-3 bg-muted shadow-sm">
@@ -14,19 +15,42 @@ export default function Navbar() {
                 </div>
                 <span className="text-slate-800 font-bold text-xl tracking-wide">Travorra</span>
             </div>
-            <div className="search_feature">
-
+             <div className="search_feature flex items-center gap-2 bg-white px-[6px] py-[4px] border-2 border-gray-300 rounded-full">
+                <div className="hover:bg-gray-200 transition-colors duration-200 delay-100 px-3 py-1 rounded-full cursor-pointer">
+                    Location
+                </div>
+                <div className="bg-gray-400 h-[20px] w-[0.7px]"></div>
+                <div className="hover:bg-gray-200 transition-colors duration-200 delay-100 px-3 py-1 rounded-full cursor-pointer">
+                    Date
+                </div>
+                <div className="bg-gray-400 h-[20px] w-[0.7px]"></div>
+                <div className="hover:bg-gray-200 transition-colors duration-200 delay-100 px-3 py-1 rounded-full cursor-pointer">
+                    Details
+                </div>
+                <div className="bg-slate-400 text-white p-2 rounded-full cursor-pointer hover:scale-105 transition-all duration-300 delay-100">
+                    <Search />
+                </div>
             </div>
             <div className="">
                 <UserComponent />
             </div>
-            
         </div>
     );
 }
 
 const UserComponent = () => {
-    return(
-        <CircleUserRound className="text-slate-800"/>
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger className="outline-none">
+                <CircleUserRound className="text-slate-800" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>My Bookings</DropdownMenuItem>
+                <DropdownMenuItem>My Favorites</DropdownMenuItem>
+                <DropdownMenuItem>My Properties</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Travorra your home</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
